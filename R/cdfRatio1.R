3688c46f40d77b86a5ad153f7707f128604a8787
Ioannis Nicolis <ioannis.nicolis@u-paris.fr>
2022-03-14 14:42:49 +0100

    vector input for cdfRatio

--- R/cdfRatio1.R
+++ R/cdfRatio1.R
@@ -1,6 +1,6 @@
 #' Cumulative Distribution Function of the ratio of two normal variables
 #'
-#' @param x a quantile value or a vector of quantile values
+#' @param x a quantile value
 #' @param mu1 mean of numerator
 #' @param mu2 mean of denominator
 #' @param sigma1 standard deviation of numerator
@@ -8,16 +8,22 @@
 #' @param rho correlation coefficient between numerator and denominator
 #'
 #' @return cumulative distribution function at quantile x
-#' @export
 #' @importFrom mvtnorm pmvnorm
 #'
 #' @examples
 #' cdfRatio(x = 2.3, mu1 = 5, mu2 = 2.5, sigma1 = 1, sigma2 = 0.5, rho = 0.4)
 
-cdfRatio <- function(x,mu1,mu2,sigma1,sigma2,rho){
-  if(length(x)==1)
-    cdf <- cdfRatio1(x,mu1,mu2,sigma1,sigma2,rho)
-  else
-    cdf <- cdfRatioN(x,mu1,mu2,sigma1,sigma2,rho)
+cdfRatio1 <- function(x,mu1,mu2,sigma1,sigma2,rho){
+  b <- mu2/sigma2
+  cr <- sqrt(1/(1-rho^2))
+  a <- cr*(mu1/sigma1-rho*b)
+  tx <- cr*(x*sigma2/sigma1-rho)
+  st2 <- sqrt(1+tx^2)
+  bas1 <- (a-b*tx)/st2
+  bas2 <- -b
+  rhoRatio <- tx/st2
+  rhoRatiomat <- matrix(c(1,rhoRatio,rhoRatio,1),nrow=2)
+  cdf <- pmvnorm(lower=c(bas1,bas2),corr=rhoRatiomat,keepAttr = F) +
+    pmvnorm(lower = c(-bas1,-bas2),corr=rhoRatiomat,keepAttr = F)
   return(cdf)
 }
--- R/cdfRatioN.R
+++ R/cdfRatioN.R
@@ -8,16 +8,13 @@
 #' @param rho correlation coefficient between numerator and denominator
 #'
 #' @return cumulative distribution function at quantile x
-#' @export
 #' @importFrom mvtnorm pmvnorm
+#' @importFrom purrr map_dbl
 #'
 #' @examples
-#' cdfRatio(x = 2.3, mu1 = 5, mu2 = 2.5, sigma1 = 1, sigma2 = 0.5, rho = 0.4)
+#' cdfRatio(x = c(1.4,2.3,3.6), mu1 = 5, mu2 = 2.5, sigma1 = 1, sigma2 = 0.5, rho = 0.4)
 
-cdfRatio <- function(x,mu1,mu2,sigma1,sigma2,rho){
-  if(length(x)==1)
-    cdf <- cdfRatio1(x,mu1,mu2,sigma1,sigma2,rho)
-  else
-    cdf <- cdfRatioN(x,mu1,mu2,sigma1,sigma2,rho)
+cdfRatioN <- function(x,mu1,mu2,sigma1,sigma2,rho){
+  cdf <- x %>% map_dbl(cdfRatio1,mu1,mu2,sigma1,sigma2,rho)
   return(cdf)
 }
